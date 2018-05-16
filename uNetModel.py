7d988a82a72f4cc24d111388cb965b25b1cbe58d
javiray619 <34381738+javiray619@users.noreply.github.com>
2018-05-16 02:55:05 -0700

    Fixed model and started training on it!

--- uNetModel.py
+++ uNetModel.py
@@ -41,28 +41,28 @@ class UNet(nn.Module):
         self.convT61 = nn.ConvTranspose2d(in_channels = 128, out_channels =  64, kernel_size = 2, stride = 2, output_padding = 0)
         nn.init.kaiming_normal_(self.convT61.weight)
         
-        self.conv63 = nn.Conv2d(in_channels = 64, out_channels = 64, kernel_size = 3, padding = pad, groups = 2)
+        self.conv63 = nn.Conv2d(in_channels = 128, out_channels = 64, kernel_size = 3, padding = pad)
         nn.init.kaiming_normal_(self.conv63.weight)
         self.conv65 = nn.Conv2d(in_channels = 64, out_channels = 64, kernel_size = 3, padding = pad)
         nn.init.kaiming_normal_(self.conv65.weight)
         self.convT71 = nn.ConvTranspose2d(in_channels = 64, out_channels =  32, kernel_size = 2, stride = 2, output_padding = 0)
         nn.init.kaiming_normal_(self.convT71.weight)
         
-        self.conv73 = nn.Conv2d(in_channels = 32, out_channels = 32, kernel_size = 3, padding = pad, groups = 2)
+        self.conv73 = nn.Conv2d(in_channels = 64, out_channels = 32, kernel_size = 3, padding = pad)
         nn.init.kaiming_normal_(self.conv73.weight)
         self.conv75 = nn.Conv2d(in_channels = 32, out_channels = 32, kernel_size = 3, padding = pad)
         nn.init.kaiming_normal_(self.conv75.weight)
         self.convT81 = nn.ConvTranspose2d(in_channels = 32, out_channels =  16, kernel_size = 2, stride = 2, output_padding = 0)
         nn.init.kaiming_normal_(self.convT81.weight)
         
-        self.conv83 = nn.Conv2d(in_channels = 16, out_channels = 16, kernel_size = 3, padding = pad, groups = 2)
+        self.conv83 = nn.Conv2d(in_channels = 32, out_channels = 16, kernel_size = 3, padding = pad)
         nn.init.kaiming_normal_(self.conv83.weight)
         self.conv85 = nn.Conv2d(in_channels = 16, out_channels = 16, kernel_size = 3, padding = pad)
         nn.init.kaiming_normal_(self.conv85.weight)
         self.convT91 = nn.ConvTranspose2d(in_channels = 16, out_channels =  8, kernel_size = 2, stride = 2, output_padding = 0)
         nn.init.kaiming_normal_(self.convT91.weight)
         
-        self.conv93 = nn.Conv2d(in_channels = 8, out_channels = 8, kernel_size = 3, padding = pad, groups = 2)
+        self.conv93 = nn.Conv2d(in_channels = 16, out_channels = 8, kernel_size = 3, padding = pad)
         nn.init.kaiming_normal_(self.conv93.weight)
         self.conv95 = nn.Conv2d(in_channels = 8, out_channels = 8, kernel_size = 3, padding = pad)
         nn.init.kaiming_normal_(self.conv95.weight)
@@ -91,19 +91,20 @@ class UNet(nn.Module):
         x14 = self.conv53(F.relu(x13))
 
         x15 = self.convT61(F.relu(x14))
-        x16 = self.conv63(torch.stack((x11, x15), 1))
+        x16_input = torch.cat((x11, x15), 1)
+        x16 = self.conv63(x16_input)
         x17 = self.conv65(F.relu(x16))
 
         x18 = self.convT71(F.relu(x17))
-        x19 = self.conv73(torch.stack((x8, x18), 1))
+        x19 = self.conv73(torch.cat((x8, x18), 1))
         x20 = self.conv75(F.relu(x19))
 
         x21 = self.convT81(F.relu(x20))
-        x22 = self.conv83(torch.stack((x5, x21), 1))
+        x22 = self.conv83(torch.cat((x5, x21), 1))
         x23 = self.conv85(F.relu(x22))
 
         x24 = self.convT91(F.relu(x23))
-        x25 = self.conv93(torch.stack((x2, x24), 1))
+        x25 = self.conv93(torch.cat((x2, x24), 1))
         x26 = self.conv95(F.relu(x25))
 
         x27 = self.conv96(F.relu(x26))
